62a91cc3e481b0826c46f039f4dec370920e01a6
Luis Dutra <luis.dutra.001@acad.pucrs.br>
2024-07-11 18:23:36 -0300

    Clean solution and add SeedProducts

---
+++ VirtualShop/VirtualShop.ProductApi/Migrations/20240711211159_SeedProducts.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtualShop.ProductApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Products(Name, Price, Description, Stock, ImageURL, CategoryId) " +
+                                 "Values('Caderno', 7.55, 'Caderno Credeal', 10, 'caderno1.jpg', 1)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name, Price, Description, Stock, ImageURL, CategoryId) " +
+                                 "Values('Lápis', 3.45, 'Lápis Faber Castell', 20, 'lapis1.jpg', 1)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name, Price, Description, Stock, ImageURL, CategoryId) " +
+                                 "Values('Clips', 5.33, 'Clips Tilibra', 50, 'clips1.jpg', 2)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Products");
+        }
+    }
+}
