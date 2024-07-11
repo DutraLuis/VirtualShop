bb9f34b90e1044abf0b2a7223881856533fd7ef0
Luis Dutra <luis.dutra.001@acad.pucrs.br>
2024-07-11 18:01:33 -0300

    Update context class and add new initial migration

--- VirtualShop/VirtualShop.ProductApi/obj/Debug/net8.0/VirtualShop.ProductApi.AssemblyInfo.cs
+++ VirtualShop/VirtualShop.ProductApi/obj/Debug/net8.0/VirtualShop.ProductApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VirtualShop.ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a765ed11ee9c317baf21b2b780dda7c6ca81fca1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b8d27c180a4eeb735ad08f9e1d2f4526223d5ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("VirtualShop.ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VirtualShop.ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
