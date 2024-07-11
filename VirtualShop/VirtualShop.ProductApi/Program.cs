0b12b158fc0efbe321caacc1b1484efbe32ac454
Luis Dutra <luis.dutra.001@acad.pucrs.br>
2024-07-11 16:08:57 -0300

    config context on services

--- VirtualShop/VirtualShop.ProductApi/Program.cs
+++ VirtualShop/VirtualShop.ProductApi/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using VirtualShop.ProductApi.Context;
+
 namespace VirtualShop.ProductApi
 {
     public class Program
@@ -13,6 +16,12 @@ namespace VirtualShop.ProductApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            var mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            builder.Services.AddDbContext<ApplicationContext>(options => options
+                   .UseMySql(mySqlConnection, ServerVersion
+                   .AutoDetect(mySqlConnection)));
 
             var app = builder.Build();
 
