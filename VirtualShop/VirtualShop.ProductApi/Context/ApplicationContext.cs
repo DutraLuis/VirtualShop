ef73e64786bfccad33dba47eed0f1256ce1aa04c
Luis Dutra <luis.dutra.001@acad.pucrs.br>
2024-07-11 13:59:03 -0300

    add context class

---
+++ VirtualShop/VirtualShop.ProductApi/Context/ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VirtualShop.ProductApi.Models;
+
+namespace VirtualShop.ProductApi.Context;
+
+public class ApplicationContext : DbContext
+{
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+}
