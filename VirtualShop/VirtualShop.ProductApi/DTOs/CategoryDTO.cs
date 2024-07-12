e148987564c48a4143c522aa3ed7f4a2e6c9cad8
Luis Dutra <luis.dutra.001@acad.pucrs.br>
2024-07-12 17:26:33 -0300

    add DTO classes

---
+++ VirtualShop/VirtualShop.ProductApi/DTOs/CategoryDTO.cs
@@ -0,0 +1,10 @@
+﻿using VirtualShop.ProductApi.Models;
+
+namespace VirtualShop.ProductApi.DTOs;
+
+public class CategoryDTO
+{
+    public int CategoryId { get; set; }
+    public string? Name { get; set; }
+    public ICollection<Product>? Products { get; set; }
+}
--- VirtualShop/VirtualShop.ProductApi/obj/Debug/net8.0/VirtualShop.ProductApi.AssemblyInfo.cs
+++ VirtualShop/VirtualShop.ProductApi/obj/Debug/net8.0/VirtualShop.ProductApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VirtualShop.ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62a91cc3e481b0826c46f039f4dec370920e01a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d2c2b0c841d94641a764b243f67637589d65187")]
 [assembly: System.Reflection.AssemblyProductAttribute("VirtualShop.ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VirtualShop.ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
