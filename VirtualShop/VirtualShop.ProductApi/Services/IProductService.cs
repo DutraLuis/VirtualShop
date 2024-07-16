a151e79a463085a3e35101cc43ff80c9082d7ed5
Luis Dutra <luis.dutra.001@acad.pucrs.br>
2024-07-16 11:16:31 -0300

    implement ProductService

--- VirtualShop/VirtualShop.ProductApi/Services/IProductService.cs
+++ VirtualShop/VirtualShop.ProductApi/Services/IProductService.cs
@@ -1,5 +1,12 @@
-﻿namespace VirtualShop.ProductApi.Services;
+﻿using VirtualShop.ProductApi.DTOs;
 
-public class IProductService
+namespace VirtualShop.ProductApi.Services;
+
+public interface IProductService
 {
+    Task<IEnumerable<ProductDTO>> GetProducts();
+    Task<ProductDTO> GetProductById(int id);
+    Task AddProduct(ProductDTO productDTO);
+    Task UpdateProduct(ProductDTO productDTO);
+    Task RemoveProduct(int id);
 }
