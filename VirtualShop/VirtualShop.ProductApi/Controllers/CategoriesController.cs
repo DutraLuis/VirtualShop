9f677c069dbffaf50fc962ce4a0ea332b33688f7
Luis Dutra <luis.dutra.001@acad.pucrs.br>
2024-07-17 12:07:34 -0300

    implement controllers

--- VirtualShop/VirtualShop.ProductApi/Controllers/CategoriesController.cs
+++ VirtualShop/VirtualShop.ProductApi/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VirtualShop.ProductApi.DTOs;
 using VirtualShop.ProductApi.Services;
 
 namespace VirtualShop.ProductApi.Controllers;
@@ -12,5 +13,76 @@ public class CategoriesController : ControllerBase
     public CategoriesController(ICategoryService categoryService)
     {
         _categoryService = categoryService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+    {
+        var categoriesDTO = await _categoryService.GetCategories();
+
+        if (categoriesDTO == null)
+            return NotFound("Categories not found");
+
+        return Ok(categoriesDTO);
+    }
+
+    [HttpGet("products")]
+    public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetCategoriesProducts()
+    {
+        var categoriesDTO = await _categoryService.GetCategoriesProducts();
+
+        if (categoriesDTO == null)
+            return NotFound("Categories not found");
+
+        return Ok(categoriesDTO);
+    }
+
+    [HttpGet("{id:int}", Name = "GetCategory")]
+    public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetById(int id)
+    {
+        var categoryDTO = await _categoryService.GetCategoryById(id);
+
+        if (categoryDTO == null)
+            return NotFound("Category not found");
+
+        return Ok(categoryDTO);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateCategory([FromBody] CategoryDTO categoryDTO)
+    {
+        if (categoryDTO == null)
+            return BadRequest("Invalid Data");
+
+        await _categoryService.AddCategory(categoryDTO);
+
+        return new CreatedAtRouteResult("GetCategory", new { id = categoryDTO.CategoryId }, categoryDTO);
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> UpdateCategory(int id, [FromBody] CategoryDTO categoryDTO)
+    {
+        if (id != categoryDTO.CategoryId)
+            return BadRequest("Incorrect id");
+
+        if (categoryDTO == null)
+            return BadRequest("Invalid Data");
+
+        await _categoryService.UpdateCategory(categoryDTO);
+
+        return Ok(categoryDTO);
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult<CategoryDTO>> DeleteCategory(int id)
+    {
+        var categoryDto = await _categoryService.GetCategoryById(id);
+
+        if (categoryDto == null)
+            return NotFound("Category not found");
+
+        await _categoryService.RemoveCategory(id);
+
+        return Ok(categoryDto);
     }
 }
--- VirtualShop/VirtualShop.ProductApi/Controllers/ProductsController.cs
+++ VirtualShop/VirtualShop.ProductApi/Controllers/ProductsController.cs
@@ -1,11 +1,77 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using VirtualShop.ProductApi.DTOs;
+using VirtualShop.ProductApi.Services;
 
-namespace VirtualShop.ProductApi.Controllers
+namespace VirtualShop.ProductApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductsController : ControllerBase
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService)
     {
+        _productService = productService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ProductDTO>>> GetProducts()
+    {
+        var productsDto = await _productService.GetProducts();
+
+        if (productsDto == null)
+            return NotFound("Products not found");
+
+        return Ok(productsDto);
+    }
+
+    [HttpGet("{id}", Name = "GetProductById")]
+    public async Task<ActionResult<ProductDTO>> GetById(int id)
+    {
+        var productDto = await _productService.GetProductById(id);
+
+        if (productDto == null)
+            return NotFound("Product not found");
+
+        return Ok(productDto);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateProduct([FromBody] ProductDTO productDto)
+    {
+        if (productDto == null)
+            return BadRequest("Invalid Data");
+
+        await _productService.AddProduct(productDto);
+
+        return new CreatedAtRouteResult("GetProduct", new { id = productDto.Id }, productDto);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateProduct(int id, [FromBody] ProductDTO productDto)
+    {
+        if (id != productDto.Id)
+            return BadRequest("Incorrect id");
+
+        if (productDto == null)
+            return BadRequest("Invalid Data");
+
+        await _productService.UpdateProduct(productDto);
+
+        return Ok(productDto);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<ProductDTO>> DeleteProduct(int id)
+    {
+        var productDto = await _productService.GetProductById(id);
+
+        if (productDto == null)
+            return NotFound("Product not found");
+
+        await _productService.RemoveProduct(id);
+
+        return Ok(productDto);
     }
 }
